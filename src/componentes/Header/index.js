141334cd0eb4f373aa4af8f8792e55dab315c39e
JRSgit <jronaldsantos@hotmail.com>
2019-11-02 01:41:52 -0300

    Alterar HeaderIndex

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 const Header = () => (
   <header id='main-header'>
     <div>JRSIPTV</div>
-    <a href="">Teste da Lista</a>
+    <a href="https://www.meuteste.me/7063bb88f405fe424e88a257fe281190-7d425bfc2b138a52a190c9a0ea5c9fe0">Teste da Lista</a>
     </header>
 );
 
